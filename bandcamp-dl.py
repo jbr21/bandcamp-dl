b37b32d5345f239baa29ddd32c6218962574a0d0
Jack <inanoplay@gmail.com>
2019-05-11 12:05:33 +0100

    Code cleanup and amending varying mp3-128 links
    As well as realisation that Bandcamp serves the mp3 from the mp3-128 link with any tags the mp3 was uploaded with, so the get_tags and set_tags functions would have been completely redundant.
    
    I might re-add them in the future if I end up making checks to see if the downloaded mp3 already has tags, and if not to set them using the album data.

--- bandcamp-dl.py
+++ bandcamp-dl.py
@@ -5,41 +5,32 @@ import urllib.request
 import shutil
 from bs4 import BeautifulSoup
 import json
-# import mutagen
 
 request_header = {
     'User-Agent': "Mozilla/5.0 (X11; Linux x86_64; rv:12.0) Gecko/20100101 Firefox/12.0"
 }
 
-song_url_regex = r"(https:\/\/t4.bcbits.com\/stream\/[a-z0-9]{32}\/mp3-128\/[0-9]{10}\?p=[0-9]&ts=[0-9]{10}&t=[a-z0-9]{40}&token=[0-9]{10}_[a-z0-9]{40})"
+song_url_regex = r"(https:\/\/t4.bcbits.com\/stream\/[a-z0-9]{32}\/mp3-128\/[0-9]{0,12}\?p=[0-9]&ts=[0-9]{10}&t=[a-z0-9]{40}&token=[0-9]{10}_[a-z0-9]{40})"
 album_data_regex = r"TralbumData = (\{(\s*?.*?)*?\});$"
-
-def get_tags():
-    pass
-
-def set_tags():
-    pass
 
 def main():
     parser = ap.ArgumentParser(description="Download music from Bandcamp at 128kbps")
     parser.add_argument('url', action="store", help="Given URL to Bandcamp track")
     args = parser.parse_args()
 
-    r = requests.get(args.url, headers=request_header)
-    soup = BeautifulSoup(r.text, "html.parser")
+    soup = BeautifulSoup(requests.get(args.url, headers=request_header), "html.parser")
 
     script = soup.find('script', text=re.compile('TralbumData'))
-    json_text = re.sub(r"([ ]+[\/]{2}.*\n)", '', re.search(album_data_regex, script.string, flags=re.DOTALL | re.MULTILINE).group(1))
-    print(json_text)
-    # json_data = json.loads(json_text)
+    album_data = re.sub(r"([ ]+[\/]{2}.*\n)", '', re.search(album_data_regex, script.string, flags=re.DOTALL | re.MULTILINE).group(1))
+    print(album_data)
 
-    # print(soup.prettify())
+    # Dumping the HTML page into "output.html" for inspection
     # with open("output.html", "wb") as f:
         # f.write(soup.prettify().encode("utf-8"))
     
-    # song_url = re.findall(song_url_regex, html)[0]
-    # with urllib.request.urlopen(song_url) as song_response, open("out.mp3", "wb") as out_file:
-    #     shutil.copyfileobj(song_response, out_file)
+    song_url = re.findall(song_url_regex, album_data)[0]
+    with urllib.request.urlopen(song_url) as r, open("out.mp3", "wb") as f:
+        shutil.copyfileobj(r, f)
 
 if __name__ == "__main__":
     main()
